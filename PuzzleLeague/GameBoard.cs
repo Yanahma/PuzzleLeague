9e52518658f2f9741a6a420634eec972402539b4
Yanahma <evilbottom1@hotmail.co.uk>
2017-04-26 21:06:07 +0100

    Draw new gameboard graphic, added code to contain "column" details & check for matches

--- PuzzleLeague/GameBoard.cs
+++ PuzzleLeague/GameBoard.cs
@@ -8,48 +8,185 @@ namespace PuzzleLeague
 {
    class GameBoard
    {
+      //
+      // Private fields
+      //
+
+      // Hold an array of six lists, which serve as containing the data of the "columns" 
+      private List<Block>[] columns = new List<Block>[6];
+
+      // Hold an array of dirty flags for the six columns as above
+      private bool[] columnDirty = new bool[6];
+
       // The list of rows currently on the GameBoard
-      private List<Row> rows;
+      private LinkedList<Row> rows;
 
-      // This is where the start of the rows is
-      public const int GAMEBOARD_X_ANCHOR = 306;
+      // Whether or not the GameBoard is currently active
+      private bool paused = false;
 
+      //
+      // Public constants
+      //
+
+      // Where a row's X should start (used in Row constructor)
+      public const int GameBoardXAnchor = 306;
+      public const int GameBoardYHeight = 12 * Block.BlockHeight;
+
+      public int YOffSet
+      {
+         get { return (int)rows.First.Value.Position.Y % Block.BlockHeight; }
+      }
+
+      //
+      // Constructor
+      //
       public GameBoard()
       {
-         rows = new List<Row>();
+         rows = new LinkedList<Row>();
+         // Init the array of lists
+         for (var i = 0; i < columns.Length; i++)
+         {
+            columns[i] = new List<Block>();
+            columnDirty[i] = true;
+         }
       }
 
       int cnt = 0; // Temp code for adding rows!!
-      #region Game Loop lifecycle
+
+      // Main update method
       public void Update()
       {
-         if (cnt++ == 53)
+         if (cnt++ == 54 - 1)
          {
             AddRow();
             cnt = 0;
+            BuildAllColumns();
          }
 
-         foreach(Row row in rows)
-            row.Update();
+         //if (rows.Count == 13)
+         //paused = true;
+
+         if (!(paused))
+         {
+            foreach (Row row in rows)
+               row.Update();
+         }
+
+         CheckForMatches();
       }
-      
+
+      // Main draw method
       public void Draw(SpriteBatch spriteBatch)
       {
-         // Draw the background
-         spriteBatch.Draw(ContentHelper.GetTexture("gameBoardBackground"), Vector2.Zero, Color.White);
-         
+         // Draw the backgrounds (should always fill entire screen)
+         Rectangle drawTo = new Rectangle(Point.Zero, new Point(ScaleHelper.BackBufferWidth,ScaleHelper.BackBufferHeight));
+
+         spriteBatch.Draw(ContentHelper.GetTexture("gameBoardBackground"), drawTo, Color.White);
+
          // Draw each of the rows that belong to this GameBoard
          foreach (Row row in rows)
             row.Draw(spriteBatch);
 
          // Draw the background overlay (super cheaty)
-         spriteBatch.Draw(ContentHelper.GetTexture("gameBoardOverlayCheat"), Vector2.Zero, Color.White);
+         spriteBatch.Draw(ContentHelper.GetTexture("gameBoardOverlayCheat"), drawTo, Color.White);
       }
-      #endregion
 
+      // Add a random row to this GameBoard
       public void AddRow()
       {
-         rows.Add(Row.RandomRow());
+         rows.AddLast(Row.RandomRow());
+      }
+
+      // Build a single column in the columns array
+      private void BuildColumn(int i)
+      {
+         // Init column
+         columns[i].Clear();
+         columnDirty[i] = true;
+
+         // Loop through rows & add to each column
+         foreach (Row r in rows)
+         {
+            columns[i].Add(r[i]);
+         }
+      }
+
+      // Build the columns array
+      private void BuildAllColumns()
+      {
+         // Init the array of lists
+         for (var i = 0; i < columns.Length; i++)
+         {
+            columns[i].Clear();
+            columnDirty[i] = true;
+         }
+
+         // Loop through rows & add to each column
+         foreach (Row r in rows)
+         {
+            for (var i = 0; i < columns.Length; i++)
+            {
+               columns[i].Add(r[i]);
+            }
+         }
+      }
+
+      // Method to check for matches in the rows and columns (not to be confused w/ "CheckMatches")
+      private void CheckForMatches()
+      {
+         List<Block> matchedBlocks;
+
+         // Check for horizontal matches
+         foreach (Row r in rows)
+         {
+            if (r.HorizontalDirty)
+            {
+               matchedBlocks = CheckMatches(r.Blocks);
+               if (matchedBlocks.Count > 0)
+               {
+                  foreach (Block b in matchedBlocks)
+                     b.IsMatched = true;
+               }
+               r.HorizontalDirty = false;
+            }
+         }
+
+         // Check for vertical matches
+         for (var i = 0; i < columns.Length; i++)
+         {
+            if (columnDirty[i])
+            {
+               matchedBlocks = CheckMatches(columns[i]);
+               if (matchedBlocks.Count > 0)
+               {
+                  foreach (Block b in matchedBlocks)
+                     b.IsMatched = true;
+               }
+               columnDirty[i] = false;
+            }
+         }
+      }
+
+      /// <summary>
+      /// Takes a list of ordered blocks and returns a list of any matches of length 3 or greater
+      /// </summary>
+      /// <param name="blocks">List of blocks (in order they appear)</param>
+      /// <returns>Unique list of any blocks that match</returns>
+      private List<Block> CheckMatches(List<Block> blocks)
+      {
+         var matchedBlocks = new List<Block>();
+         for (var i = 0; i <= blocks.Count - 3; i++)
+         {
+            if (blocks[i].Type == blocks[i + 1].Type && blocks[i + 1].Type == blocks[i + 2].Type)
+            {
+               for (var j = i; j <= i + 2; j++)
+               {
+                  if (!(matchedBlocks.Contains(blocks[j])))
+                     matchedBlocks.Add(blocks[j]);
+               }
+            }
+         }
+         return matchedBlocks;
       }
    }
 }
