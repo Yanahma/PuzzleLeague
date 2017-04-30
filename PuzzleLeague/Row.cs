311024b6745d002c7bdd51e424b4638698ef3669
Yanahma <evilbottom1@hotmail.co.uk>
2017-04-30 17:58:20 +0100

    Code refactor to remove a lot of unnecessary properties and one-line methods

--- PuzzleLeague/Row.cs
+++ PuzzleLeague/Row.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using PuzzleLeague.Utilities;
@@ -15,18 +16,38 @@ namespace PuzzleLeague
       // Private fields
       //
 
+      // The parent GameBoard of this Row
+      private GameBoard parent;
+      
       // Array of blocks that belong to this row (0 = leftmost, 5 = rightmost)
       private Block[] blocks;
-
-      // Dirty flag for the horizontal of this row; no need to check for matches if not dirty
-      private bool horizontalDirty = true;
 
       // Defines the X,Y position of this Row
       private Vector2 position;
 
+      // Private property to return if this row is full of empty blocks
+      private bool isEmpty
+      {
+         get
+         {
+            return (blocks[0].Type == BlockType.Empty
+         && blocks[5].Type == BlockType.Empty
+         && blocks[1].Type == BlockType.Empty
+         && blocks[4].Type == BlockType.Empty
+         && blocks[2].Type == BlockType.Empty
+         && blocks[3].Type == BlockType.Empty);
+         }
+      }
+
       //
-      // Public accessors
+      // Public fields
       //
+
+      // Dirty flag for the horizontal of this row; no need to check for matches if not dirty
+      public bool isDirty = true;
+
+      // Flag to determine if this row can be affected
+      public bool isActive = false;
 
       // Public index access returns the block at index position
       public Block this[int index]
@@ -34,62 +55,64 @@ namespace PuzzleLeague
          get { return blocks[index]; }
       }
 
-      // Public accessor for 'blocks' field (convert to list)
-      public List<Block> Blocks
+      // Public accessor for 'blocks' field
+      public Block[] Blocks
       {
-         get
-         {
-            var rtnBlocks = new List<Block>();
-            foreach (Block b in blocks)
-            {
-               rtnBlocks.Add(b);
-            }
-            return rtnBlocks;
-         }
-      }
-
-      // Public accessor for "horizontalDirty" flag
-      public bool HorizontalDirty
-      {
-         get { return horizontalDirty; }
-         set { horizontalDirty = value; }
+         get { return blocks; }
       }
 
       // Public accessor for 'position' field 
-      public Vector2 Position { get { return position; } }
+      public Vector2 Position
+      {
+         get { return position; }
+      }
 
       //
       // Constructor
       //
-      public Row()
+      public Row(GameBoard parent)
       {
-         // A row is always 6 blocks
+         this.parent = parent;
          blocks = new Block[6];
 
          // Set the X position to match the gameboard anchor, the Y position to the height of the screen
          position = new Vector2(GameBoard.GameBoardXAnchor, ScaleHelper.BackBufferHeight);
       }
 
-      /// <summary>
-      /// Add a block to the blocks array
-      /// </summary>
-      /// <param name="block">Block to add to array</param>
-      /// <param name="index">Index position to add to</param>
-      public void AddBlock(Block block, int index) => blocks[index] = block;
+      // Add a block to the "blocks" array
+      public void AddBlock(Block block, int index)
+      {
+         blocks[index] = block;
+         block.SetParent(this);
+         parent.FlagColumn(index); // Flag column as dirty
+         isDirty = true; // Flag row as dirty
+      }
 
-      /// <summary>
-      /// Accessability for blocks as to the index of the slot they are in
-      /// </summary>
-      /// <param name="item">Block to retrieve the index of in the row</param>
-      /// <returns>The index in the row</returns>
-      public int IndexOfBlock(Block item) => Array.IndexOf(blocks, item);
+      // Remove a block from the "blocks" array (replace with BlockType.Empty)
+      public void RemoveBlock(int index)
+      {
+         blocks[index] = new Block(this, BlockType.Empty);
+         parent.FlagColumn(index); // Flag column as dirty
+         isDirty = true; // Flag row as dirty
+      }
+
+      // Accessability for Blocks to know which position they are in
+      public int IndexOf(Block item) => Array.IndexOf(blocks, item);
 
       // Main update method
       public void Update()
       {
-         foreach (Block b in blocks)
-            b.Update();
-         position.Y -= 1;
+         if (isEmpty)
+         {
+            parent.RemoveRow(this);
+            Array.Clear(blocks, 0, 6);
+         }
+         else
+         {
+            position.Y = ScaleHelper.BackBufferHeight - parent.YOffset - (ScaleHelper.ScaleHeight(Block.BlockHeight * parent.IndexOf(this)));
+            foreach (Block b in blocks)
+               b.Update();
+         }
       }
 
       // Main draw method
@@ -99,18 +122,39 @@ namespace PuzzleLeague
             b.Draw(spriteBatch);
       }
 
-      /// <summary>
-      /// Static generator of a random row
-      /// </summary>
-      /// <returns>A new row populated with 6 random blocks (not including empties)</returns>
-      static public Row RandomRow()
+      // Static generator of a random row (does not include empty blocks)
+      static public Row RandomRow(GameBoard parent)
       {
-         Row rndRow = new Row();
+         Row rndRow = new Row(parent);
          for (var i = 0; i <= 5; i++)
          {
             rndRow.AddBlock(Block.RndBlockExcEmpty(rndRow), i);
          }
          return rndRow;
+      }
+
+      // Display more meaningful data about this Row
+      public override string ToString()
+      {
+         StringBuilder rowString = new StringBuilder();
+         for(var i = 0; i < blocks.Length; i++)
+         {
+            rowString.AppendFormat("Index {0} = {1}", i, blocks[i].Type.ToString());
+         }
+         return rowString.ToString();
+      }
+
+      // Swap the blocks at two positions
+      public void SwapAt(int position1, int position2)
+      {
+         if (blocks[position1].CanBeSwapped
+            && blocks[position2].CanBeSwapped)
+         {
+            Block save = blocks[position1];
+            blocks[position1] = blocks[position2];
+            blocks[position2] = save;
+            isDirty = true;
+         }
       }
    }
 }
